13f736c2b74a0b6188a94b884050ee01ac582e06
Lawrence Wang <625lawrence@gmail.com>
2014-08-31 03:42:12 -0400

    remove old ADA code; document as necessary

--- code/ODNB/unsorted_code/ADA01.OldCode.R
+++ code/ODNB/unsorted_code/ADA01.OldCode.R
@@ -1,3 +1,7 @@
+##@S This contains very old code, done at the very beginning of the project. Experiments were done on only a small subset of documents (60 documents), and the ideas used here are not really applicable on data of the scale that we need... 
+
+
+
 #(ada start.R)
 
 #setwd("C:/Users/Lawrence/Desktop")
@@ -13,7 +17,7 @@ text.ind = sort(as.numeric(substr(to.scan, a+1, a+attr(a, "match.length")-1)))
 filenames = paste("ada/file_",text.ind,".txt", sep = "")
 html.files = list()
 for(i in 1:length(filenames)) {
- html.files = c(html.files, list(readLines(filenames[i])))
+  html.files = c(html.files, list(readLines(filenames[i])))
 }
 
 ## First 54 files: 
@@ -31,89 +35,89 @@ for(i in 1:length(filenames)) {
 
 text.toproc = list()
 for(i in 1:length(filenames)) {
- text.toproc = c(text.toproc, list(dnb.grab.main(html.files[[i]])))
+  text.toproc = c(text.toproc, list(dnb.grab.main(html.files[[i]])))
 }
 gregexpr(pattern = "hi", text = "obaiowehiasdfaweiofahi")
 
 dnb.grab.name <- function(art.main) {
- matches = gregexpr(pattern = "<span class=\"headword\">.+<span class=\"occ\">",  text = art.main)
- toreturn = ""
- for(i in 1:length(art.main)) {
-  posit = matches[[i]]
-  index = attr(matches[[i]], "match.length")
-  if(posit[1] > 0) {
-   for(j in 1:length(posit)) {
-    temp.sub = substr(art.main[i], start = posit[j], stop = posit[j] + index[j] - 1)
-    t1 = gsub(pattern = "\\(.*?\\)", replacement = "", x = temp.sub)
-    t2 = gsub(pattern = "<.*?>", replacement = "#", x = t1)
-    t3 = gsub(pattern = " *, +", replacement = ",", x = t2)
-    t4 = gsub(pattern = " +", replacement = " ", x = t3)
-    t5 = gsub(pattern = "#,#", replacement = "#", x = t4)
-    t6 = gsub(pattern = " *#+ *", replacement = "#", x = t5)
-    t7 = gsub(pattern = "##+", replacement = "#", x = t6)
-    toreturn = c(toreturn, t7)
-   }
+  matches = gregexpr(pattern = "<span class=\"headword\">.+<span class=\"occ\">",  text = art.main)
+  toreturn = ""
+  for(i in 1:length(art.main)) {
+    posit = matches[[i]]
+    index = attr(matches[[i]], "match.length")
+    if(posit[1] > 0) {
+      for(j in 1:length(posit)) {
+        temp.sub = substr(art.main[i], start = posit[j], stop = posit[j] + index[j] - 1)
+        t1 = gsub(pattern = "\\(.*?\\)", replacement = "", x = temp.sub)
+        t2 = gsub(pattern = "<.*?>", replacement = "#", x = t1)
+        t3 = gsub(pattern = " *, +", replacement = ",", x = t2)
+        t4 = gsub(pattern = " +", replacement = " ", x = t3)
+        t5 = gsub(pattern = "#,#", replacement = "#", x = t4)
+        t6 = gsub(pattern = " *#+ *", replacement = "#", x = t5)
+        t7 = gsub(pattern = "##+", replacement = "#", x = t6)
+        toreturn = c(toreturn, t7)
+      }
+    }
   }
- }
- return(toreturn[-1])
+  return(toreturn[-1])
 }
 returned = lapply(text.toproc, FUN = dnb.grab.name)
 list.collapse.top <- function(lis) {
- toreturn = ""
- for(i in 1:length(lis)) {
-  toreturn = c(toreturn,lis[[i]][1])
- }
- return(toreturn[-1])
+  toreturn = ""
+  for(i in 1:length(lis)) {
+    toreturn = c(toreturn,lis[[i]][1])
+  }
+  return(toreturn[-1])
 }
 coll = list.collapse.top(returned)
 regexpr(pattern = "\\[", text = "oawejh[oaw")
 
 
 find.names <- function(vect) {
- toreturn = matrix("", ncol = 7, nrow = length(vect))
- for(i in 1:length(vect)) {   ### Standard #Last,First#
-  b = gregexpr(pattern = "#[[:alpha:]]+ *[[:alpha:]]*,[[:alpha:]]+ *[[:alpha:]]*#", text = vect[i])[[1]]
-  if(b[1] > 0) {
-   t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
-   loc = regexpr(pattern = ",", text = t1)
-   last.name = substr(t1, 1, loc - 1)
-   first.name = substr(t1, loc + 1, stop = nchar(t1))
-   toreturn[i,1:2] = c(first.name,last.name)
-   vect[i] = sub(pattern = "#[[:alpha:]]+ *[[:alpha:]]*,[[:alpha:]]+ *[[:alpha:]]*#", replacement = "#", x = vect[i])
+  toreturn = matrix("", ncol = 7, nrow = length(vect))
+  for(i in 1:length(vect)) {   ### Standard #Last,First#
+    b = gregexpr(pattern = "#[[:alpha:]]+ *[[:alpha:]]*,[[:alpha:]]+ *[[:alpha:]]*#", text = vect[i])[[1]]
+    if(b[1] > 0) {
+      t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
+      loc = regexpr(pattern = ",", text = t1)
+      last.name = substr(t1, 1, loc - 1)
+      first.name = substr(t1, loc + 1, stop = nchar(t1))
+      toreturn[i,1:2] = c(first.name,last.name)
+      vect[i] = sub(pattern = "#[[:alpha:]]+ *[[:alpha:]]*,[[:alpha:]]+ *[[:alpha:]]*#", replacement = "#", x = vect[i])
+    }
   }
- }
- for(i in 1:length(vect)) {   ### # Last#[Last.Alt]#,First#
-  if (!(vect[i] == "#")) {
-   b = gregexpr(pattern = "#[[:alpha:]]+ *[[:alpha:]]*#\\[[[:alpha:]]+\\]#,[[:alpha:]]+ *[[:alpha:]]*#", text = vect[i])[[1]]
-   if(b[1] > 0) {
-    t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
-    loc = regexpr(pattern = ",", text = t1)
-    first.name = substr(t1, loc + 1, stop = nchar(t1))
-    locl = regexpr(pattern = "#", text = t1)
-    last.name = substr(t1, 1, locl - 1)
-    toreturn[i,1:2] = c(first.name,last.name)
-    alt.last = substr(t1, locl + 2, loc - 3)
-    vect[i] = sub(pattern = "#[[:alpha:]]+ *[[:alpha:]]*#\\[[[:alpha:]]+\\]#,[[:alpha:]]+ *[[:alpha:]]*#", replacement = "#", x = vect[i])
-    toreturn[i,4] = alt.last
-   }
-   b = gregexpr(pattern = "\\[[[:alpha:]]+\\]", text = vect[i])[[1]]
-   if (b[1] > 0) {
-    t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
-    toreturn[i,3] = t1
-    vect[i] = sub(pattern = "\\[[[:alpha:]]+\\]", "#", x = vect[i])
-   }
-   b = gregexpr(pattern = "\\[[[:alpha:]]+ [[:alpha:]]+\\]", text = vect[i])[[1]]
-   if (b[1] > 0) {
-    t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
-    loc = regexpr(pattern = " ", text = t1)
-    alt.first = substr(t1, 1, loc - 1)
-    alt.last = substr(t1, loc + 1, nchar(t1))
-    toreturn[i,3:4] = c(alt.first,alt.last)
-    vect[i] = sub(pattern = "\\[[[:alpha:]]+ [[:alpha:]]+\\]", "#", x = vect[i])
-   }
+  for(i in 1:length(vect)) {   ### # Last#[Last.Alt]#,First#
+    if (!(vect[i] == "#")) {
+      b = gregexpr(pattern = "#[[:alpha:]]+ *[[:alpha:]]*#\\[[[:alpha:]]+\\]#,[[:alpha:]]+ *[[:alpha:]]*#", text = vect[i])[[1]]
+      if(b[1] > 0) {
+        t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
+        loc = regexpr(pattern = ",", text = t1)
+        first.name = substr(t1, loc + 1, stop = nchar(t1))
+        locl = regexpr(pattern = "#", text = t1)
+        last.name = substr(t1, 1, locl - 1)
+        toreturn[i,1:2] = c(first.name,last.name)
+        alt.last = substr(t1, locl + 2, loc - 3)
+        vect[i] = sub(pattern = "#[[:alpha:]]+ *[[:alpha:]]*#\\[[[:alpha:]]+\\]#,[[:alpha:]]+ *[[:alpha:]]*#", replacement = "#", x = vect[i])
+        toreturn[i,4] = alt.last
+      }
+      b = gregexpr(pattern = "\\[[[:alpha:]]+\\]", text = vect[i])[[1]]
+      if (b[1] > 0) {
+        t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
+        toreturn[i,3] = t1
+        vect[i] = sub(pattern = "\\[[[:alpha:]]+\\]", "#", x = vect[i])
+      }
+      b = gregexpr(pattern = "\\[[[:alpha:]]+ [[:alpha:]]+\\]", text = vect[i])[[1]]
+      if (b[1] > 0) {
+        t1 = substr(vect[i], b[1]+1, b[1] + attr(b, "match.length")[1] - 2)
+        loc = regexpr(pattern = " ", text = t1)
+        alt.first = substr(t1, 1, loc - 1)
+        alt.last = substr(t1, loc + 1, nchar(t1))
+        toreturn[i,3:4] = c(alt.first,alt.last)
+        vect[i] = sub(pattern = "\\[[[:alpha:]]+ [[:alpha:]]+\\]", "#", x = vect[i])
+      }
+    }
   }
- }
- return(list(vect, toreturn))
+  return(list(vect, toreturn))
 }
 temp = find.names(coll)
 
@@ -133,34 +137,34 @@ num.in.bio.self = rep(0, times = 54)
 self = c(he, him, his, her, she, hers)
 
 for(i in 1:54) {
- for(j in 1:54) {
-  e = length(grep(pattern = names.mat[j,2], x = text.toproc[[i]]))
-  d = sum(e)
-  num.in.bio.last[i,j] = d
-  e = length(grep(pattern = names.mat[j,1], x = text.toproc[[i]]))
-  d = sum(e)
-  num.in.bio.first[i,j] = d
- }
+  for(j in 1:54) {
+    e = length(grep(pattern = names.mat[j,2], x = text.toproc[[i]]))
+    d = sum(e)
+    num.in.bio.last[i,j] = d
+    e = length(grep(pattern = names.mat[j,1], x = text.toproc[[i]]))
+    d = sum(e)
+    num.in.bio.first[i,j] = d
+  }
 }
 
 for(i in 1:54) {
- for(j in 1:54) {
-  d = 0
-  e = grep(pattern = names.mat[j,2], x = text.toproc[[i]])
-  f = grep(pattern = names.mat[j,1], x = text.toproc[[i]])
-  if (length(e) > 0) {
-   for(k in 1:length(e)) {
-    d = d + sum(f == e[k])
-   }
+  for(j in 1:54) {
+    d = 0
+    e = grep(pattern = names.mat[j,2], x = text.toproc[[i]])
+    f = grep(pattern = names.mat[j,1], x = text.toproc[[i]])
+    if (length(e) > 0) {
+      for(k in 1:length(e)) {
+        d = d + sum(f == e[k])
+      }
+    }
+    num.in.bio.both[i,j] = d
   }
-  num.in.bio.both[i,j] = d
- }
- print(i)
+  print(i)
 }
 
 zz = 0
 for(i in 1:54) {
- zz[i] = sum(num.in.bio.both[i,]) - num.in.bio.both[i,i] }
+  zz[i] = sum(num.in.bio.both[i,]) - num.in.bio.both[i,i] }
 
 which(zz > 5) -> rr
 num.in.bio.both[rr,rr] -> tt
@@ -172,7 +176,7 @@ names.mat[rr,1]
 names.mat[rr,2]
 ss = ""
 for(i in 1:54) {
- ss[i] = paste(names.mat[i,1], names.mat[i,2])
+  ss[i] = paste(names.mat[i,1], names.mat[i,2])
 }
 test2 = network(num.in.bio.both)
 plot.network(test2, label = ss)
@@ -185,23 +189,23 @@ plot.network(test, label = ss)
 
 num.both.close =  matrix(0, nrow = 54, ncol = 54) # number times within 15 words
 for(i in 1:54) {
- for(j in 1:54) {
-  e = grep(pattern = names.mat[j,2], x = text.toproc[[i]])
-  d = 0
-  if (length(e) > 0) { 
-   for(k in 1:length(e)) {
-    f = gregexpr(pattern = names.mat[j,2], text.toproc[[i]][e[k]])
-    g = gregexpr(pattern = names.mat[j,1], text.toproc[[i]][e[k]])
-    if (g[[1]][1] > 0) {
-     for(l in 1:length(f)) {
-      if (sum(abs(f[[1]][l] - g[[1]]) < 15) > 0) { d = d + 1 } 
-     }
+  for(j in 1:54) {
+    e = grep(pattern = names.mat[j,2], x = text.toproc[[i]])
+    d = 0
+    if (length(e) > 0) { 
+      for(k in 1:length(e)) {
+        f = gregexpr(pattern = names.mat[j,2], text.toproc[[i]][e[k]])
+        g = gregexpr(pattern = names.mat[j,1], text.toproc[[i]][e[k]])
+        if (g[[1]][1] > 0) {
+          for(l in 1:length(f)) {
+            if (sum(abs(f[[1]][l] - g[[1]]) < 15) > 0) { d = d + 1 } 
+          }
+        }
+      }
     }
-   }
+    num.both.close[i,j] = d
   }
-  num.both.close[i,j] = d
- }
- print(i)
+  print(i)
 }
 
 yy = network(num.both.close)
@@ -224,9 +228,7 @@ hist(h[h>0], main = "Histogram of non-diagonal entries of B", xlab = "Value of B
 proc
 
 
- [1] "<span class=\"headword\">Bernard,  Richard</span> <span class=\"occ\">
 
-}
 #lapply(text.toproc, FUN = function(x) {sapply(x, FUN = substr, start = 1, stop = 50, USE.NAMES=FALSE)})
 
 
@@ -261,17 +263,17 @@ test2 = dnb.grab.main(testfile)
 #relabel.html <- function(text) {
 # temp = text
 # temp = gsub("&#145;", "\'", temp)
-# temp = gsub("&#146;", "\'", temp)
-# temp = gsub("&#150;", "-", temp)
-# temp = gsub("&#151;", "-", temp)
-# temp = gsub("%20", " ", temp)
-# temp = gsub("%2C", ",", temp)
-# temp = gsub("%3F", "-", temp)
-# temp = gsub("<br>", " ", temp)
-# temp = gsub("<a.*?</h2>", " ", temp)
-# temp = gsub("<.*?>", " ", temp)
-# temp = gsub("\\( d .", "\\(died", temp)
-# return(temp[-length(temp)])
+  # temp = gsub("&#146;", "\'", temp)
+  # temp = gsub("&#150;", "-", temp)
+  # temp = gsub("&#151;", "-", temp)
+  # temp = gsub("%20", " ", temp)
+  # temp = gsub("%2C", ",", temp)
+  # temp = gsub("%3F", "-", temp)
+  # temp = gsub("<br>", " ", temp)
+  # temp = gsub("<a.*?</h2>", " ", temp)
+  # temp = gsub("<.*?>", " ", temp)
+  # temp = gsub("\\( d .", "\\(died", temp)
+  # return(temp[-length(temp)])
 #}
 
 bb = relabel.html(test2)
@@ -285,79 +287,79 @@ substr(be[[1]], zz, zz + attr(zz, "mat"))
 substr(be[[1]], 1, zz-1)
 
 split.vector = function(vec) { # returns matrix
- zz = regexpr("/.*?$", vec) 
- tags = substr(vec, zz, zz + attr(zz, "mat"))
- text = substr(vec, 1, zz-1)
- return(rbind(text,tags))
+  zz = regexpr("/.*?$", vec) 
+  tags = substr(vec, zz, zz + attr(zz, "mat"))
+  text = substr(vec, 1, zz-1)
+  return(rbind(text,tags))
 }
 
 split.list = function(lis) { #return list of split.vector matrices
- toreturn = list()
- for(i in 1:length(lis)) {
-  toreturn = c(toreturn, list(split.vector(lis[[i]])))
- }
- return(toreturn)
+  toreturn = list()
+  for(i in 1:length(lis)) {
+    toreturn = c(toreturn, list(split.vector(lis[[i]])))
+  }
+  return(toreturn)
 }
 bf = split.list(be)
 
 extract.noun.seq = function(lis) { #input: list of matrices
- ## Columns:
- # 1. combined text
- # 2. tag
- # 3. sent. location start
- # 4. sent. location end
- # 5. sentence
- toreturn = matrix(NA, nrow = length(lis)*10, ncol = 5)
- index = 1
- for(i in 1:length(lis)) {
-  pron = grep("/PRP", lis[[i]][2,])
-  if (length(pron) > 0) {
-   for (j in 1:length(pron) ) { 
-    toreturn[index,] = c(lis[[i]][1,pron[j]], lis[[i]][2,pron[j]], pron[j], pron[j], i)
-    index = index + 1
-   }
+  ## Columns:
+  # 1. combined text
+  # 2. tag
+  # 3. sent. location start
+  # 4. sent. location end
+  # 5. sentence
+  toreturn = matrix(NA, nrow = length(lis)*10, ncol = 5)
+  index = 1
+  for(i in 1:length(lis)) {
+    pron = grep("/PRP", lis[[i]][2,])
+    if (length(pron) > 0) {
+      for (j in 1:length(pron) ) { 
+        toreturn[index,] = c(lis[[i]][1,pron[j]], lis[[i]][2,pron[j]], pron[j], pron[j], i)
+        index = index + 1
+      }
+    }
+    nnp = grep("/NNP", lis[[i]][2,])
+    if (length(nnp) > 0) {
+      nnp.mat = find.seq(nnp)
+      #   print(nnp.mat)
+      for(j in 2:dim(nnp.mat)[1]) {
+        toreturn[index,] = c( 
+          paste(lis[[i]][1,nnp.mat[j,1]:nnp.mat[j,2]], collapse = " "), 
+          "/NNP", 
+          nnp.mat[j,1], nnp.mat[j,2], i)
+        index = index + 1
+      }
+    }
   }
-  nnp = grep("/NNP", lis[[i]][2,])
-  if (length(nnp) > 0) {
-   nnp.mat = find.seq(nnp)
-   #   print(nnp.mat)
-   for(j in 2:dim(nnp.mat)[1]) {
-    toreturn[index,] = c( 
-                 paste(lis[[i]][1,nnp.mat[j,1]:nnp.mat[j,2]], collapse = " "), 
-                 "/NNP", 
-                 nnp.mat[j,1], nnp.mat[j,2], i)
-    index = index + 1
-   }
-  }
- }
- return(toreturn[1:index,])
+  return(toreturn[1:index,])
 }
 
 extract.noun.seq(bf)
 
 find.seq = function(vec) { #output = matrix of start/end, 2 cols
- cp.vec = vec
- ret = matrix(NA,nrow = 50, ncol = 2)
- ind = 1
- last = -10
- for(i in 1:length(vec)) {
-  if(cp.vec[1] == last+1) {
-   ret[ind,2] = cp.vec[1]
-   last = cp.vec[1]
-   cp.vec = cp.vec[-1]
-  } else {
-   if (ind > 0) {
-    if (is.na(ret[ind,2])) { ret[ind,2] = ret[ind,1] }
-   }
-   ind = ind + 1
-   ret[ind,1] = cp.vec[1]
-   last = cp.vec[1]
-   cp.vec = cp.vec[-1]
+  cp.vec = vec
+  ret = matrix(NA,nrow = 50, ncol = 2)
+  ind = 1
+  last = -10
+  for(i in 1:length(vec)) {
+    if(cp.vec[1] == last+1) {
+      ret[ind,2] = cp.vec[1]
+      last = cp.vec[1]
+      cp.vec = cp.vec[-1]
+    } else {
+      if (ind > 0) {
+        if (is.na(ret[ind,2])) { ret[ind,2] = ret[ind,1] }
+      }
+      ind = ind + 1
+      ret[ind,1] = cp.vec[1]
+      last = cp.vec[1]
+      cp.vec = cp.vec[-1]
+    }
   }
- }
- if(is.na(ret[ind,2])) { ret[ind,2] = ret[ind,1] }
-
- return(ret[1:ind,])
+  if(is.na(ret[ind,2])) { ret[ind,2] = ret[ind,1] }
+  
+  return(ret[1:ind,])
 }
 
 tt = grep("/NNP", bf[[16]][2,])
@@ -365,25 +367,25 @@ tt = grep("/NNP", bf[[16]][2,])
 tokenize(bd)
 list.mats = list()
 for(i in 1:54) {
- a1 = relabel.html(text.toproc[[i]])
- a2 = sentDetect(a1)
- a3 = tagPOS(a2)
- a4 = strsplit(a3, split = " ")
- a5 = split.list(a4)
- a6 = extract.noun.seq(a5) 
- list.mats = c(list.mats, list(a6))
- print(i)
+  a1 = relabel.html(text.toproc[[i]])
+  a2 = sentDetect(a1)
+  a3 = tagPOS(a2)
+  a4 = strsplit(a3, split = " ")
+  a5 = split.list(a4)
+  a6 = extract.noun.seq(a5) 
+  list.mats = c(list.mats, list(a6))
+  print(i)
 }
 
 rm.pron.etc <- function(lis) {
- tmp = list()
- for(i in 1:length(lis)) {
-  test = lis[[i]][-dim(lis[[i]])[1],]
-  test = test[test[,2] == "/NNP",]
-  
-  tmp = c(tmp, list(test)) 
- } 
- return(tmp)
+  tmp = list()
+  for(i in 1:length(lis)) {
+    test = lis[[i]][-dim(lis[[i]])[1],]
+    test = test[test[,2] == "/NNP",]
+    
+    tmp = c(tmp, list(test)) 
+  } 
+  return(tmp)
 }
 list.names = rm.pron.etc(list.mats)
 
