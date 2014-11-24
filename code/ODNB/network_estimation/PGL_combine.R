75d8cafe8417d1a19d9c5149046984d2f3db4a75
Lawrence Wang <625lawrence@gmail.com>
2014-11-24 01:32:32 -0500

    edit PGL fitting for parallel machines.

---
+++ code/ODNB/network_estimation/PGL_combine.R
@@ -0,0 +1,28 @@
+files = list.files("TEMP/", full.names = TRUE)
+files = files[grep(files, pattern = "SS\\_")]
+
+library(Matrix)
+
+load(files[1])
+res_matrix = Matrix(0, sparse = TRUE, nrow = nrow(lambda_matrix), ncol = ncol(lambda_matrix))
+for(j in seq_along(files)) {
+  print(j)
+  load(files[j])
+  res_matrix = res_matrix + (lambda_matrix + t(lambda_matrix) > 0)
+}
+res_matrix = round(res_matrix / length(files),2)
+
+table(res_matrix[12935,])
+sum(res_matrix)
+
+load("data_manual/ODNB_dataset.Rdata")
+
+nodeset[grep("Harrington", nodeset$search_names_ALL),]
+
+k = 8309 # Milton
+k = 5566 # Harrington
+sum(res_matrix[k,] == res_matrix[,k])
+table(res_matrix[k,])
+z = which(res_matrix[k,] > 0)
+z = z[order(res_matrix[k,z])]
+cbind(nodeset$full_name[z], nodeset$full_date[z], res_matrix[k,z])
--- code/ODNB/network_estimation/PGL_fit.R
+++ code/ODNB/network_estimation/PGL_fit.R
@@ -1,110 +1,117 @@
 ## Run fitting for currently specified iteration numbers
-## Before running, make sure "trials" exists: 
+## Before running, make sure "TRIALS" exists:
+## Also, need "NCORES" exists
 
-if("TRIALS" %in% ls() && length(TRIALS) == 2) {
+## Can use "RESTRICT" to restrict to only a subset of nodes. 
+
+log_result = function(...) {
+  cat(...)
+  cat(..., file = trial_log, append = TRUE)
+}
+
+if ((all(c("NCORES", "TRIALS") %in% ls()) && all(TRIALS > 0))) {
   library(glmnet)
+  library(parallel)
   
-  START_TRIAL = TRIALS[1]
-  END_TRIAL = TRIALS[2]
-
+  if (FALSE) {
+    TRIALS = 1; NCORES = 4; RESTRICT = 5500:5599
+    TRIALS = 1:3
+    NCORES = 1
+    RESTRICT = 5500:5505
+  }
+  
   LAMBDA_DEPTH = 3
   source("code/ODNB/network_estimation/PGL_setup.R")
   load("data/ODNB_final/nodeset_overlap_list.Rdata")
   ALLOWED_INDS = date_overlap_list
   
   SAVE_RESULTS = FALSE
-  FILENAME_model = "TEMP/SS_newfit"
-  RECORD_TIMING = NULL
+  FILENAME_model = "TEMP/SS_newfit_pos_"
   MODE = "date-limited"
-  MAX_REITERS = 100 
+  MAX_REITERS = 25 
+  MAX_BATCHSIZE = 100
   NODES = length(ALLOWED_INDS)
   
-  for(nn in START_TRIAL:END_TRIAL) {
+  for(nn in TRIALS) {
     trial_log = paste("TEMP/trial_",nn,".log", sep = "")
-    cat("New run of PGL, starting at: ", date(), file = trial_log, append = TRUE)
+    filename_save_lambdas = paste(FILENAME_model,nn,".Rdata", sep = "")
     
+    log_result("New run of PGL, starting at: ", date(), "\n")
     
     ##### First pass through the data
-    cat("Starting Trial",nn,"of",END_TRIAL,"\n", file = trial_log, append = TRUE)
-    
-    cat("-- Creating subset of data matrix","\n", file = trial_log, append = TRUE)
+    log_result("Starting Trial",nn,"\n")
+    log_result("-- Creating subset of data matrix","\n")
     load(paste("data/ODNB_newfinal/sampmatrix",nn,".Rdata", sep = ""))
     SUBSET_DM = dcmat
     
-    filename_save_lambdas = paste(FILENAME_model,nn,".Rdata", sep = "")
     results = list()
-    
     for(run in 1:MAX_REITERS) {
-      cat("==========\n", file = trial_log, append = TRUE)
+      log_result("==========\n")
       
+      ## Figure out progress on all nodes
       if (run == 1) {
         progress = list()
         remaining_nodes = 1:NODES
+        if ("RESTRICT" %in% ls()) {remaining_nodes = intersect(remaining_nodes, RESTRICT)}
       } else {
-        cat("Checking progress and processing nodes for next run... \n", file = trial_log, append = TRUE)
-        
-        remaining_nodes = which(sapply(results, function(y) {y$status == "InProg"}))
-        
+        log_result("Checking progress and processing nodes for next run... \n")
+        remaining_nodes = which(sapply(results, function(y) {!is.null(y) && (y$status == "InProg")}))
         progress = list()
         if (length(remaining_nodes) > 0) {
-          for (j in remaining_nodes) {
-            progress[[j]] = list(lambda = results[[j]]$lambda, iters=(run-1))
-          }
-        } else {
-          break
-        }  
+          progress[remaining_nodes] = lapply(remaining_nodes, function(j) { list(lambda = results[[j]]$lambda, iters = (run - 1)) })
+        } else { break }
       }
       
-      cat(length(remaining_nodes),"nodes left. \n", file = trial_log, append = TRUE)
-      cat("==========\n", file = trial_log, append = TRUE)
-      
+      log_result(length(remaining_nodes),"nodes left. \n","==========\n")
       # If there are still nodes left, continue. (otherwise, could would have went to 'break')
-      current_run = NULL # like before, current_run
       
-      for(i in remaining_nodes) {
-        cat("Trial: ", nn, "ITER:", run, " ", date(), "    ======    Nodes:",i, "\n", sep = "")
-        cat("Trial: ", nn, "ITER:", run, " ", date(), "    ======    Nodes:",i, "\n",sep = "",
-            file = trial_log, append = TRUE)
-        results[[i]] = glm_x(x = i, prog = progress, run_no = run, fit_mode = MODE, target_node = TARGET_NODE)
+      #       ## Single iteration version. 
+      #       for(i in remaining_nodes) {
+      #         log_result("Trial: ", nn, "ITER:", run, " ", date(), "    ======    Nodes:",i, "\n", sep = ""
+      #         results[[i]] = glm_x(x = i, prog = progress, run_no = run, fit_mode = MODE, target_node = TARGET_NODE)
+      #       }
+      ## Using parallel version
+      batches = split(x = remaining_nodes, f = rep(1:ceiling(NODES / MAX_BATCHSIZE), each = MAX_BATCHSIZE)[seq_along(remaining_nodes)])
+      for(bat in seq_along(batches)) {
+        log_result("Trial:",nn, "-ITER:",run,"-----BATCH:", bat, "/", length(batches),"-----     -----", date(), "\n", sep = "")
+        results[batches[[bat]]] = mclapply(X = batches[[bat]], FUN = function(y) { glm_x(x = y, prog = progress, run_no = run, fit_mode = MODE, target_node = TARGET_NODE) }, mc.cores = NCORES)
       }
     }
+    log_result("------------GLM Fitting all done\n")
     
-    print("GLM ALL RAN")
-
     # Process errors: 
-    errors = which(sapply(results, function(y) {y$status == "Error"}))
-    for (j in errors) {
-      results[[j]]$status2 = process_error_results(j)
-    }
+    errors = which(sapply(results, function(y) {!is.null(y) && y$status == "Error"}))
+    for (j in errors) { results[[j]]$status2 = process_error_results(j) }
     
     cat("Extracting Lambda values... \n", file = trial_log, append = TRUE)
     lambda_matrix = Matrix(0, nrow = NODES, ncol = NODES, sparse = TRUE)
     
     error_presence = FALSE
-    for(j in 1:NODES) {
+    for(j in which(sapply(results, function(x) {!is.null(x)}))) {
       if (results[[j]]$status == "Error") {
         if (results[[j]]$status2 == "column_all_zeros") {
+          ## No need; no fitting done here. 
         } else {
-          cat("Trial",nn,": Error = Column",j,"has error message that is not column_all_zeros. \n", file = trial_log, append = TRUE)
+          log_result("Trial",nn,": Error = Column",j,"has error message that is not column_all_zeros. \n")
           error_presence = TRUE
         }
       } else if (results[[j]]$status == "Done") {
         temp = results[[j]]$results
-        lambda_matrix[j,temp$nodeID] = temp$lambda
-        if (j %% 1000 == 0) { print(j) }
+        ## Keep only positive coefficients > 0.001
+        poscoefs = which(temp$finalBeta > 0.001)
+        temp = temp[poscoefs,]
+        if (length(poscoefs) > 0) { lambda_matrix[j,as.numeric(temp$name)] = temp$lambda}
       } else {
-        cat("Trial",nn,": Error = Column",j,"does not have status Done or Error. \n", file = trial_log, append = TRUE)
+        log_result("Trial",nn,": Error = Column",j,"does not have status Done or Error. \n")
         error_presence = TRUE
       }
     }
     
-    if (error_presence) { cat("ERROR IN TRIAL",nn,"OF",END_TRIAL,"\n", file = trial_log, append = TRUE) }
+    if (error_presence) { log_result("ERROR IN TRIAL",nn,"OF",END_TRIAL,"\n") }
     
     save(lambda_matrix, file = filename_save_lambdas)
-    cat("Finished Trial",nn,"of",END_TRIAL,"\n", file = trial_log, append = TRUE)
-    cat("--------------------------------------------------", file = trial_log, append = TRUE)
+    log_result("Finished Trial",nn,"-----", date(), "\n", "----------------------\n")
   }
-  
 } else {
-  print("Must specify variable TRIALS")
+  print("Must specify variable TRIALS and NCORES")
 }
