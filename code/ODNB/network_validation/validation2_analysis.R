c776490c98bb8452dcd79dcc9b897ab8b9a72c73
Lawrence Wang <625lawrence@gmail.com>
2015-03-08 22:57:31 -0400

    rewrite code; delete old lengthy code.

--- code/ODNB/network_validation/validation2_analysis.R
+++ code/ODNB/network_validation/validation2_analysis.R
@@ -6,6 +6,7 @@
 jh_links = read.csv("data_vc/validation2/datajh.csv", header = TRUE, stringsAsFactors = FALSE)[,c(1,2,3)]
 jm_links = read.csv("data_vc/validation2/datajm.csv", header = TRUE, stringsAsFactors = FALSE)[,c(1,2,3,4)]
 jm_links = jm_links[which(jm_links[,1] == "John Milton"), c(2,3,4)]
+jm_links[,3] = as.numeric(jm_links[,3])
 raw_ranks = read.csv("data_vc/validation2/rank_raw.csv", header = TRUE, stringsAsFactors = FALSE)
 
 colnames(jh_links) = c("Name", "ID", "Ranking")
@@ -21,201 +22,65 @@ jm_ranks = raw_ranks[which(raw_ranks$Main.Person == "John Milton"), ]
 jm_ranks = jm_ranks[match(jm_links$Name, jm_ranks$Link), ]
 nrow(jm_ranks) == sum(jm_ranks$Link == jm_links$Name) # check that names match up
 
-
-
-
-
-# analysis
-tosave = read.csv("test2.csv")
-load("cb500i1.dm.Rdata")
-
-jh.dat = read.csv("datajh.csv")
-jh.dat = jh.dat[,2:3]
-jh = which(tosave[,2] == "James Harrington")
-main.dat = tosave[jh,-1]
-main.ranks = matrix(NA, nrow = 71, ncol = 24)
-p.dat = jh.dat
-
-jm.dat = read.csv("datajm.csv")
-jm.dat = jm.dat[1:111,3:4]
-jm = which(tosave[,2] == "John Milton")
-main.dat = tosave[jm,-1]
-main.ranks = matrix(NA, nrow = 111, ncol = 24)
-p.dat = jm.dat
-## -- re-written up to here
-
-for(r in 1:dim(main.ranks)[1]) {
-  for(c in 4:27) {
-    if (!is.na(main.dat[r,c])) {
-      if (main.dat[r,c] == 0) {
-        main.dat[r,c] = NA
-      }
-    }
-  }
+## Change method values into a ranking, where larger numbers are better. 
+for(c in 4:27) {
+  jm_ranks[,c] = rank(jm_ranks[,c], na.last = "keep", ties.method = "average")
+  jh_ranks[,c] = rank(jh_ranks[,c], na.last = "keep", ties.method = "average")
 }
 
-rank(main.dat[,4], na.last = "keep", ties.method = "average")
-for(j in 4:27) {
-  main.ranks[,j-3] = rank(main.dat[,j], na.last = "keep", ties.method = "average")
-  # making only top 30
-  #   a = sort(main.ranks[,j-3], decreasing = TRUE)
-  #   smallest.tokeep = a[min(30, length(a))]
-  #   main.ranks[(main.ranks[,j-3] < smallest.tokeep),j-3] = NA
-}
-p.dat[match( main.dat[,3],p.dat[,1]),2]
-true.ranks = rank( (101 - p.dat[match( main.dat[,3],p.dat[,1]),2]), na.last = "keep", ties.method = "average")
+jm_links = cbind(jm_links, Ranked = rank((101 - jm_links$Ranking), na.last = "keep", ties.method = "average"))
+jh_links = cbind(jh_links, Ranked = rank((101 - jh_links$Ranking), na.last = "keep", ties.method = "average"))
 
-# 1st row = spearman corr for each method
-cor.res.matrix = matrix(NA, nrow = 50, ncol = 24)
-for(j in 1:24) {
-  cor.res.matrix[1,j] = cor(true.ranks,main.ranks[,j], use = "pairwise.complete.obs")
+## Compute Spearman correlations -- basically, look at how consistent two rankings are. 
+spear_cors = matrix(0, nrow = 24, ncol = 2)
+ 
+for(j in 4:24) { 
+  spear_cors[j,1] = cor(jm_links$Ranked, jm_ranks[,j], use = "pairwise.complete.obs", method = "spearman")
+  spear_cors[j,2] = cor(jh_links$Ranked, jh_ranks[,j], use = "pairwise.complete.obs", method = "spearman")
 }
 
-# 2nd row = spearman cor for each method, ignoring negative final wtd edges
-temp.dat = main.dat[,4:27]
-temp.ranks = temp.dat
-for(j in 1:4) {
-  b = which(temp.dat[,5 + (j-1) * 6] < 0)
-  temp.dat[b,(1:6) + 6*(j-1)] = NA
-}
-for(j in 1:24) {
-  temp.ranks[,j] = rank(temp.dat[,j], na.last = "keep", ties.method = "average")
-}
-for(j in 1:24) {
-  cor.res.matrix[2,j] = cor(true.ranks,temp.ranks[,j], use = "pairwise.complete.obs")
-}
 
-#3rd row = leave blank
-#4th row = NA for 100s
-#5th row = # of 100s in top 20
-#6th row = # of 100s in top 30
-a = p.dat[match( main.dat[,3],p.dat[,1]),]
-for (k in which(a[,2] == 100)) {
-  a[k,2] = NA
-}
-true.ranks.n100 = rank( (101 - a[,2]), na.last = "keep", ties.method = "average")
-a = p.dat[match( main.dat[,3],p.dat[,1]),]
-ohs = which(a[,2] == 100)
-
+# Function that returns the indices (in order) of the top k matches. (Larger = better ranked)
 topk = function(dat, k) {
   k = min(k, sum(!is.na(dat)))
   return(order(dat,decreasing= TRUE)[1:k])
 }
-for(j in 1:24) {
-  cor.res.matrix[4,j] = cor(true.ranks.n100,main.ranks[,j], use = "pairwise.complete.obs")
-  cor.res.matrix[5,j] = length(intersect(ohs, topk(main.dat[,3+j], k = 20)))
-  cor.res.matrix[6,j] = length(intersect(ohs, topk(main.dat[,3+j], k = 30)))
-}
 
-
-# 8-10: same except 80,100 instead of just 100. 
-#3rd row = leave blank
-#4th row = NA for 100s
-#5th row = # of 100s in top 20
-#6th row = # of 100s in top 30
-a = p.dat[match( main.dat[,3],p.dat[,1]),]
-for (k in which(a[,2] > 70)) {
-  a[k,2] = NA
-}
-true.ranks.n100 = rank( (101 - a[,2]), na.last = "keep", ties.method = "average")
-a = p.dat[match( main.dat[,3],p.dat[,1]),]
-ohs = which(a[,2] > 70)
-
-topk = function(dat, k) {
-  k = min(k, sum(!is.na(dat)))
-  return(order(dat,decreasing= TRUE)[1:k])
-}
-for(j in 1:24) {
-  cor.res.matrix[8,j] = cor(true.ranks.n100,main.ranks[,j], use = "pairwise.complete.obs")
-  cor.res.matrix[9,j] = length(intersect(ohs, topk(main.dat[,3+j], k = 20)))
-  cor.res.matrix[10,j] = length(intersect(ohs, topk(main.dat[,3+j], k = 30)))
-}
-
-
-
-save(cor.res.matrix, file = "spear.cor.res.Rdata")
-
-
-tosave = read.csv("test2.csv")
-load("cb500i1.dm.Rdata")
-
-to.analyze = 1:2
-par(mfrow = c(1,2))
-
-jh.dat = read.csv("datajh.csv")
-jh.dat = jh.dat[,2:3]
-jh = which(tosave[,2] == "James Harrington")
-main.dat = tosave[jh,-1]
-main.ranks = matrix(NA, nrow = 71, ncol = 24)
-p.dat = jh.dat
-p.dat = p.dat[match( main.dat[,3],p.dat[,1]),]
-
-
-for(r in 1:dim(main.ranks)[1]) {
-  for(c in 4:27) {
-    if (!is.na(main.dat[r,c])) {
-      if (main.dat[r,c] == 0) {
-        main.dat[r,c] = NA
-      }
+# Function that counts the number of matches of a certain type, vs. when they occur (top 10, top 20, etc.)
+count_matches = function(est_ranks, orig_ranks, cuts_s = c(0, 20, 50, 80, 100), cuts_e = c(19, 49, 79, 99, 500)) {
+  # est_ranks = rank vector whose match to the correct rankings which should be computed
+  # orig_ranks = correct ranking vector
+  # cuts_s = vector of starting points of cuts
+  # cuts_e = vector of ending points of cuts
+  # The best ranking would be all orig_ranks falling inside cuts_s[1] <= x <= cuts_e[1]
+  
+  # Returns a matrix of dimension: nrows = length(est_ranks), ncol = length(cuts_s) + 1
+  # The matrix tells how many matches of the different types have occured up to the kth observation (and this information is stored in row k)
+  #   The last column in the matrix tells how many NA rankings have been estimated up to the kth row. 
+  
+  resmatrix = matrix(0, nrow = sum(!is.na(est_ranks)), ncol = length(cuts_s) + 1)
+  for(k in seq_len(sum(!is.na(est_ranks)))) {
+    topk_ranked = topk(est_ranks, k = k)
+    actual_ranks = orig_ranks[topk_ranked]
+    
+    ## Do counting
+    for(c in 1:length(cuts_s)) {
+      resmatrix[k,c] = sum(actual_ranks >= cuts_s[c] & actual_ranks <= cuts_e[c], na.rm = TRUE)
     }
+    resmatrix[k,length(cuts_s) + 1] = sum(is.na(actual_ranks))
   }
-}
-rank(main.dat[,4], na.last = "keep", ties.method = "average")
-for(j in 4:27) {
-  main.ranks[,j-3] = rank(main.dat[,j], na.last = "keep", ties.method = "average")
-  # making only top 30
-  #   a = sort(main.ranks[,j-3], decreasing = TRUE)
-  #   smallest.tokeep = a[min(30, length(a))]
-  #   main.ranks[(main.ranks[,j-3] < smallest.tokeep),j-3] = NA
-}
-p.dat[match( main.dat[,3],p.dat[,1]),2]
-true.ranks = rank( (101 - p.dat[match( main.dat[,3],p.dat[,1]),2]), na.last = "keep", ties.method = "average")
-
-topk(main.dat[,3], k = 5)
-
-# counting type of match vs. when they occur (top 10, top 20, etc)
-ks = 1:50
-cuts.s = c(0, 20, 50, 80, 100)
-cuts.e = c(19, 49, 79, 99, 500)
-count.res.matrix = array(NA, dim = c(length(ks), length(cuts.s)+1, 3))
-
-# plot for nosp
-to.try = 6
-for(k in 1:length(ks)) {
-  finds = topk(main.dat[,to.try], k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,1] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,1] = sum(is.na(assigns))
+  return(resmatrix)
 }
 
-
-# plot for sp500
-to.try = 12
-negs = which(main.dat[,14] < 0)
-temp = main.dat[,to.try]
-temp[negs] = NA
-for(k in 1:length(ks)) {
-  finds = topk(temp, k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,2] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,2] = sum(is.na(assigns))
-}
+# test
+# count_matches(est_ranks = jm_ranks[,6], orig_ranks = jm_links$Ranking)
 
 
-#plot for corr
-to.try = 15
-for(k in 1:length(ks)) {
-  finds = topk(main.dat[,to.try], k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,3] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,3] = sum(is.na(assigns))
-}
+
+## TODO: Redo plot below
+
+
+
 
 plot(x = -1,y = -100, xlim = c(0,50), ylim = c(0,40), xlab = "Top x", 
      ylab = "Number of Top x correct", main = "James Harrington")
@@ -231,96 +96,4 @@ legend(x = "topleft", col = 1:3, lty = 1,
        legend = c("PGL full docs", "PGL split-docs", "Corr split-docs"),
        cex = 0.7)
 
-
-
-
-
-## John Milton
-
-
-
-
-jm.dat = read.csv("datajm.csv")
-jm.dat = jm.dat[1:111,3:4]
-jm = which(tosave[,2] == "John Milton")
-main.dat = tosave[jm,-1]
-main.ranks = matrix(NA, nrow = 111, ncol = 24)
-p.dat = jm.dat
-
-for(r in 1:dim(main.ranks)[1]) {
-  for(c in 4:27) {
-    if (!is.na(main.dat[r,c])) {
-      if (main.dat[r,c] == 0) {
-        main.dat[r,c] = NA
-      }
-    }
-  }
-}
-rank(main.dat[,4], na.last = "keep", ties.method = "average")
-for(j in 4:27) {
-  main.ranks[,j-3] = rank(main.dat[,j], na.last = "keep", ties.method = "average")
-  # making only top 30
-  #   a = sort(main.ranks[,j-3], decreasing = TRUE)
-  #   smallest.tokeep = a[min(30, length(a))]
-  #   main.ranks[(main.ranks[,j-3] < smallest.tokeep),j-3] = NA
-}
-p.dat[match( main.dat[,3],p.dat[,1]),2]
-true.ranks = rank( (101 - p.dat[match( main.dat[,3],p.dat[,1]),2]), na.last = "keep", ties.method = "average")
-
-topk(main.dat[,3], k = 5)
-
-# counting type of match vs. when they occur (top 10, top 20, etc)
-ks = 1:50
-cuts.s = c(0, 20, 50, 80, 100)
-cuts.e = c(19, 49, 79, 99, 500)
-count.res.matrix = array(NA, dim = c(length(ks), length(cuts.s)+1, 3))
-
-# plot for nosp
-to.try = 6
-for(k in 1:length(ks)) {
-  finds = topk(main.dat[,to.try], k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,1] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,1] = sum(is.na(assigns))
-}
-
-
-# plot for sp500
-to.try = 12
-negs = which(main.dat[,14] < 0)
-temp = main.dat[,to.try]
-temp[negs] = NA
-for(k in 1:length(ks)) {
-  finds = topk(temp, k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,2] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,2] = sum(is.na(assigns))
-}
-
-
-#plot for corr
-to.try = 15
-for(k in 1:length(ks)) {
-  finds = topk(main.dat[,to.try], k = ks[k])
-  assigns = p.dat[finds,2]
-  for(c in 1:length(cuts.s)) {
-    count.res.matrix[k,c,3] = sum(assigns >= cuts.s[c] & assigns <= cuts.e[c], na.rm = 1)
-  }
-  count.res.matrix[k,length(cuts.s)+1,3] = sum(is.na(assigns))
-}
-
-plot(x = -1,y = -100, xlim = c(0,50), ylim = c(0,40), xlab = "Top x", 
-     ylab = "Number of Top x correct", main = "John Milton")
-#|----##../ --Tue Feb 24 17:29:13 2015--
-for(k in 1:3) {
-  
-  xs = rowSums(count.res.matrix[,,k])
-  ys = rowSums(count.res.matrix[,to.analyze,k, drop = FALSE])
-  points(xs, ys, type = "l", col = k)
-  
-}
 
