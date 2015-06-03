598d741bd562f32bddf5ebce5c7e9c74cd3c41c8
Lawrence Wang <625lawrence@gmail.com>
2015-06-03 13:12:10 -0400

    fix package name

--- code/compile_SDFBFunctions.R
+++ code/compile_SDFBFunctions.R
@@ -1,6 +1,6 @@
 ## To do package code processing (not necessary unless you edit the source files, and even then, it's not necessary as you can manually do most of it), you need packages metacode/stringr. These are freely available except 'metacode' which is a suite of functions that I've written to make my work easier...
 
-library(metacode)
+library(codeProcessing)
 library(stringr)
 update_fx_documentation(FD = FilesDescription(dirlist = "code/SDFBFunctions/"))
 
