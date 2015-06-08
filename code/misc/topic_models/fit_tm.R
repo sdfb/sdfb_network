5edd6345c873f33fbb6ef6b389f44c8f1e239b36
Lawrence Wang <625lawrence@gmail.com>
2015-06-08 10:22:07 -0400

    clean tm code

--- code/misc/topic_models/fit_tm.R
+++ code/misc/topic_models/fit_tm.R
@@ -3,13 +3,11 @@ library(topicmodels)
 library(tm)
 
 load("data/TOPIC_MODEL/clean_corpus_20150608.Rdata")
-print(date())
 print("Working on raw 5")
 tm_raw_5 <- LDA(x=dtm, k = 5)
 save(tm_raw_5, file = "data/TOPIC_MODEL/tms1.Rdata")
 
 print(date())
-date()
 print("Working on stem 5")
 tm_stem_5 = LDA(x = stem_dtm, k = 5)
 save(tm_raw_5, tm_stem_5, file = "data/TOPIC_MODEL/tms1.Rdata")
--- code/misc/topic_models/fit_tm2.R
+++ code/misc/topic_models/fit_tm2.R
@@ -7,19 +7,16 @@ load("data/TOPIC_MODEL/clean_corpus_20150608.Rdata")
 tm_stem_5 = NULL; tm_stem_10 = NULL; tm_stem_20 = NULL
 
 print(date())
-date()
 print("Working on stem 5")
 tm_stem_5 = LDA(x = sl_dtm, k = 5)
 save(tm_stem_5, tm_stem_10, tm_stem_20, file = "data/TOPIC_MODEL/tms2.Rdata")
 
 print(date())
-date()
 print("Working on stem 10")
 tm_stem_10 = LDA(x = sl_dtm, k = 10)
 save(tm_stem_5, tm_stem_10, tm_stem_20, file = "data/TOPIC_MODEL/tms2.Rdata")
 
 print(date())
-date()
 print("Working on stem 10")
 tm_stem_20 = LDA(x = sl_dtm, k = 20)
 save(tm_stem_5, tm_stem_10, tm_stem_20, file = "data/TOPIC_MODEL/tms2.Rdata")
