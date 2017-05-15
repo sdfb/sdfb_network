6a33c74b89d32af0eb3d0ede16890e413bcfd5a7
walling <walling@tacc.utexas.edu>
2017-05-15 11:05:05 -0500

    Improved version of parallel search

--- code/JSTOR/text_processing/search_products-files.sh
+++ code/JSTOR/text_processing/search_products-files.sh
@@ -6,22 +6,13 @@
 
 # Input
 JSTOR_PATH=/data/JSTOR/test
-NAMES_FILE=./names_scott_version.csv
+NAMES_FILE=./NamesToSearch-6bacon4.11.17.csv
 
 # Temp and Output
 MATCHING_FILES=/data/JSTOR/matching_files.txt
-UNIQUE_MATCHING_FILES=/data/JSTOR/uniq_matching_files.txt
-CONTENT_OUTPUT=/data/JSTOR/content.txt
 
 # First use parallel grep to find all files matching at least 1 of the names.
 echo "grepping $NAMES_FILE at $JSTOR_PATH"
 # Output the filename and matched regex name as: /path/to/filename.txt:regex_name
-cat $NAMES_FILE | parallel -n4 --pipe grep -o -f - $JSTOR_PATH/* | tee $MATCHING_FILES
-
-# Results are not unique, make them so
-#echo "unique $MATCHING_FILES to $UNIQUE_MATCHING_FILES"
-#cat $MATCHING_FILES | sort | uniq > $UNIQUE_MATCHING_FILES
-
-# Now save contents of all matching files into single file
-#echo catting
-#cat $UNIQUE_MATCHING_FILES | xargs cat > $CONTENT_OUTPUT
+#cat $NAMES_FILE | parallel -n4 --pipe grep -o -f - $JSTOR_PATH/* | tee $MATCHING_FILES
+ls $JSTOR_PATH/* | parallel grep -o -F -H -f $NAMES_FILE | tee $MATCHING_FILES
