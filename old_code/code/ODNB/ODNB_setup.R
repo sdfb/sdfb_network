43b57eea300b7c0e270c79a442444e6f00f34e3e
walling <walling@tacc.utexas.edu>
2017-08-23 13:32:10 -0500

    Adding back possibly missed files to master branch in old_code

---
+++ old_code/code/ODNB/ODNB_setup.R
@@ -0,0 +1,19 @@
+#@S This file runs setup for all the pertinent code files
+
+## Load pertinent libraries
+
+## Load SDFB library or helper files. 
+use_library = FALSE
+
+if (use_library) {
+  library(SDFBFunctions)
+} else {
+  source("code/SDFBFunctions/R/clean_HTML_functions.R")
+  source("code/SDFBFunctions/R/NER_processing.R")
+  source("code/SDFBFunctions/R/NER_improve.R")
+}
+
+## Load the file location variables. 
+source("code/ODNB/ODNB_file_locations.R")
+rm(use_library)
+
