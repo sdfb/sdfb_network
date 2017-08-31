996922ed4212245ac4dd706431982d0a84d74ca8
walling <walling@tacc.utexas.edu>
2017-08-31 08:20:11 -0500

    Adding json_converter.R to hpc branch.

---
+++ text_processing/json_converter.R
@@ -0,0 +1,21 @@
+# Convert json version of document counts to a matrix that can be used by network estimation code
+library(jsonlite)
+library(Laurae)
+library(data.table)
+library(parallel)
+
+
+json.data = read_json("data/dataset2.json")
+
+# Each entry in the json represents a document
+# Each document contains a list of entities in that document
+doc.ids = names(json.data)
+data.tables.list <- mclapply(1:length(json.data), function(idx) {
+  doc = doc.ids[[idx]]
+  persons = json.data[[idx]]
+  data = cbindlist(persons)
+  row.names(data) = doc
+  return(data)
+})
+
+data = rbindlist(data.tables.list, fill=TRUE)
